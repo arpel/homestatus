d66fad5cb638c5220ca4580c3daab22fcc463a0b
arpel <arpel@arpel-VirtualBox.(none)>
2013-10-13 11:49:18 +0200

    Update Dygraph & heating zone viz

--- js/homestatus.js
+++ js/homestatus.js
@@ -123,6 +123,11 @@ function Tempmonitor(feedelement){
     if (type == "D3"){
       self.graphtype = 3;
     }
+    else if(type == 10){
+      self.graphtype = 10;
+      self.toggledata = [];
+      self.heating = false;
+    }
   }
 
   Graph.prototype.getData = function()
@@ -158,7 +163,7 @@ function Tempmonitor(feedelement){
 
             if((self.graphtype == 3) && (self.graphdata[marker] === undefined)){
               self.graphdata[marker] = [];
-              if(toggle)
+              if(toggle && self.graphtype != 10)
                 self.graphdata["tt"] = [];
             }
 
@@ -188,13 +193,31 @@ function Tempmonitor(feedelement){
                   if(toggle)
                   {
                     current = parseFloat(data.datapoints[i].value);
-                    if(current > (previous + 0.1)) { self.graphdata[data.datapoints[i].at]["tt"] = 10; nbH++; }
-                    else { self.graphdata[data.datapoints[i].at]["tt"] = 0; nbL++; }
+                    if(self.graphtype == 10){
+                      if(current > (previous + 0.15)) { 
+                        if(self.heating == false){
+                          self.toggledata.push([data.datapoints[i].at]);
+                          self.heating = true;
+                        }
+                        nbH++; 
+                      }else {
+                        if(self.heating == true){
+                          self.toggledata[self.toggledata.length-1].push(data.datapoints[i].at);
+                          self.heating = false;
+                        }
+                        nbL++;
+                      }
+                    }
+                    else {
+                      if(current > (previous + 0.15)) { self.graphdata[data.datapoints[i].at]["tt"] = 10; nbH++; }
+                      else { self.graphdata[data.datapoints[i].at]["tt"] = 0; nbL++; }
+                    }
                     previous = current;
                   }
                 }
               }
             }
+            
             console.log("Low : %d | High : %d", nbL, nbH);
             self.nbH += nbH;
             self.nbL += nbL;
@@ -358,4 +381,138 @@ function Tempmonitor(feedelement){
     })(this));
 
   }
-  
+
+   Graph.prototype.getAllDataGenSlidingGraphDygraphNew = function()
+  {
+    $.when.apply(this, this.getData()).done( ( function(self) {
+      return function () {
+        //console.log(self.graphdata);
+        
+        markers = self.markers;
+        labels = self.labels;
+
+        if(self.toggle) {
+          markers.push("tt");
+          labels.push("Toggle");
+        }
+
+        $("#"+self.target).append("<div class=\"noroll\" id=\"slidingDygraph_"+self.target+"\"></div>");
+        
+        data = [];
+
+        for (di in self.graphdata) 
+        {
+          item = [ new Date(self.graphdata[di].y.substring(0, 20)+"000000Z") ];
+          for(t in markers)
+          {
+            if(self.graphdata[di][markers[t]] !== undefined)
+              item.push(parseFloat(self.graphdata[di][markers[t]]));
+            else
+              item.push(null);
+          }
+          data.push(item);
+        };
+
+        //console.log(data);
+
+        data.sort( function compareFunction(a, b){
+          return a[0] - b[0];
+        });
+
+        //console.log(data);
+
+        g1 = new Dygraph(
+          document.getElementById("slidingDygraph_"+self.target),
+          data,
+          {
+            //title: 'Daily Temperatures in New York vs. San Francisco',
+            ylabel: 'Température (C)',
+            labels: ["x"].concat(labels),
+            legend: 'always',
+            labelsDivStyles: { 'textAlign': 'right' },
+            showRangeSelector: true,
+            rangeSelectorPlotFillColor: "",
+            connectSeparatedPoints: true,
+            underlayCallback: function(canvas, area, g) {
+              canvas.fillStyle = "rgba(255, 30, 0, 0.5)";
+
+              function highlight_period(x_start, x_end) {
+                var canvas_left_x = g.toDomXCoord(x_start);
+                var canvas_right_x = g.toDomXCoord(x_end);
+                var canvas_width = canvas_right_x - canvas_left_x;
+                canvas.fillRect(canvas_left_x, area.y, canvas_width, area.h);
+              }
+
+              console.log(self.toggledata);
+
+              for (zi in self.toggledata){
+                if(self.toggledata[zi].length == 2)
+                {
+                  highlight_period(new Date(self.toggledata[zi][0]), new Date(self.toggledata[zi][1]));
+                }
+              }
+            }
+          }
+        );
+
+        /* Some stats */
+        $("#"+self.target).append("<p class=\"stats\"> On : "+self.nbH+" | Off : "+self.nbL+" | % On : "+(self.nbH*100/(self.nbH+self.nbL))+"</p>");
+      }
+    })(this));
+
+  }
+
+  // var g = new Dygraph(
+  //       document.getElementById("div_g"),
+  //       data,
+  //       {
+  //         labels: ['Date','Value'],
+  //         underlayCallback: function(canvas, area, g) {
+
+  //           canvas.fillStyle = "rgba(255, 255, 102, 1.0)";
+
+  //           function highlight_period(x_start, x_end) {
+  //             var canvas_left_x = g.toDomXCoord(x_start);
+  //             var canvas_right_x = g.toDomXCoord(x_end);
+  //             var canvas_width = canvas_right_x - canvas_left_x;
+  //             canvas.fillRect(canvas_left_x, area.y, canvas_width, area.h);
+  //           }
+
+  //           var min_data_x = g.getValue(0,0);
+  //           var max_data_x = g.getValue(g.numRows()-1,0);
+
+  //           // get day of week
+  //           var d = new Date(min_data_x);
+  //           var dow = d.getUTCDay();
+  //           var ds = d.toUTCString();
+
+  //           var w = min_data_x;
+  //           // starting on Sunday is a special case
+  //           if (dow == 0) {
+  //             highlight_period(w,w+12*3600*1000);
+  //           }
+  //           // find first saturday
+  //           while (dow != 6) {
+  //             w += 24*3600*1000;
+  //             d = new Date(w);
+  //             dow = d.getUTCDay();
+  //           }
+  //           // shift back 1/2 day to center highlight around the point for the day
+  //           w -= 12*3600*1000;  
+  //           while (w < max_data_x) {
+  //             var start_x_highlight = w;
+  //             var end_x_highlight = w + 2*24*3600*1000;
+  //             // make sure we don't try to plot outside the graph
+  //             if (start_x_highlight < min_data_x) {
+  //               start_x_highlight = min_data_x;
+  //             }
+  //             if (end_x_highlight > max_data_x) {
+  //               end_x_highlight = max_data_x;
+  //             }
+  //             highlight_period(start_x_highlight,end_x_highlight);
+  //             // calculate start of highlight for next Saturday 
+  //             w += 7*24*3600*1000;
+  //           }
+  //         }
+  //       });
+  //   }
